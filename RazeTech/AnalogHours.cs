957c92ead16ffe2b5ab7bc5ccea3640265869271
Ramūnas Mažeikis <ramunas.mazeikis62@gmail.com>
2022-06-17 13:14:08 +0300

    Initial commit.
    Signed-off-by: Ramūnas Mažeikis <ramunas.mazeikis62@gmail.com>

---
+++ RazeTech/AnalogHours.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazeTech
+{
+    struct AnalogHours
+    {
+        public byte Hours { get; private set; }
+        public byte Minutes { get; private set; }
+
+        public AnalogHours(byte hours, byte minutes)
+        {
+            bool hoursOutOfRange = hours > 12;
+            bool minutesOutOfRange = minutes > 60;
+
+            if (hoursOutOfRange && !minutesOutOfRange)
+            {
+                throw new ArgumentException("Hours cannot be greater than 12!");
+            }
+
+            if (!hoursOutOfRange && minutesOutOfRange)
+            {
+                throw new ArgumentException("Minutes cannot be greater than 60!");
+            }
+
+            if (hoursOutOfRange && minutesOutOfRange)
+            {
+                throw new ArgumentException("Hours cannot be greater than 12 and minutes greater than 60!");
+            }
+
+            this.Hours = hours;
+            this.Minutes = minutes;
+        }
+
+        public static AnalogHours FromString(string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            string[] parts = str.Split(":");
+
+            if (parts.Length > 2)
+            {
+                throw new FormatException(
+                    "Analog hour string must be of the following format: "
+                    +   "<Hours>:<Minutes>\r\n"
+                    +   "Whitespace is ignored"
+                    );
+            }
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                parts[i] = parts[i].Trim();
+            }
+
+            byte hours = byte.Parse(parts[0]);
+            byte minutes = 0;
+
+            if (parts.Length == 2)
+            {
+                minutes = byte.Parse(parts[1]);
+            }
+
+            return new AnalogHours(hours, minutes);
+        }
+    }
+}
---
+++ RazeTech/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RazeTech
+{
+    internal static class AnalogHoursExt
+    {
+        private const double DEGREES_PER_HOUR = 360.0 / 12.0;
+        private const double DEGREES_PER_MINUTE = 360.0 / 60.0;
+
+        public static double MinuteAngle(this AnalogHours hours)
+        {
+            return hours.Minutes * DEGREES_PER_MINUTE;
+        }
+
+        public static double HoursAngle(this AnalogHours hours)
+        {
+            return hours.Hours * DEGREES_PER_HOUR;
+        }
+    }
+
+    class Program
+    {
+        private static double MinimumAngleDifference(double angle1, double angle2)
+        {
+            angle1 %= 360.0;
+            angle2 %= 360.0;
+
+            double absDiff = Math.Abs(angle1 - angle2);
+
+            if (absDiff > 180.0)
+            {
+                return 360.0 - absDiff;
+            }
+            else
+            {
+                return absDiff;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                try
+                {
+                    AnalogHours hours = AnalogHours.FromString(args[i]);
+
+                    double hourAngle = hours.HoursAngle();
+                    double minuteAngle = hours.MinuteAngle();
+                    double diff = MinimumAngleDifference(hourAngle, minuteAngle);
+
+                    Console.WriteLine(args[i] + "\t" + diff);
+                }
+                catch (FormatException ex)
+                {
+                    Console.Error.WriteLine("Invalid analog hour format: " + args[i]);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.Error.WriteLine("Analog hours out of range: " + args[i]);
+                }
+            }
+        }
+    }
+}
